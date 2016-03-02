de14a75a7d1c546df51402dc50f6fa060227ffaa
“Klammertime” <audrey.ck@gmail.com>
2016-03-02 01:38:50 -0800

    feat: add Google map as ko.bindingHandlers

--- js/map.js
+++ js/map.js
@@ -19,6 +19,53 @@ $(function () {
       this.review = null;
   };
 
+   ko.bindingHandlers.googleMap = {
+        init: function(element, valueAccessor) {
+            var myLatLng = new google.maps.LatLng(37.734883, -122.363663);
+            var mapOptions = {
+                center: myLatLng,
+                zoom: 11,
+                disableDefaultUI: true,
+                zoomControl: true,
+                panControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                rotateControl: true,
+                overviewMapControl: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var initialCenter = mapOptions.center;
+            var initialZoom = mapOptions.zoom;
+            var map = new google.maps.Map(element, mapOptions);
+            var geocoder = new google.maps.Geocoder();
+
+
+            addGoToInitialExtent(map, initialCenter, initialZoom);
+
+            google.maps.event.addDomListener(map, 'idle', function() {
+                center = map.getCenter();
+            });
+
+            // when right click, go back to initial center
+            function addGoToInitialExtent(map, initialCenter, initialZoom) {
+                google.maps.event.addListener(map, 'rightclick', function() {
+                    map.setCenter(initialCenter);
+                    map.setZoom(initialZoom);
+                });
+            }
+        },
+        update: function(element, valueAccessor, allBindings) {
+            var value = valueAccessor;
+            window.addEventListener('resize', (function() {
+                map.setCenter(center);
+            }), false);
+
+            google.maps.event.addDomListener(map, 'click', function() {
+                center = map.getCenter();
+            });
+        }
+    };
+
   my.vm = function() {
     var metadata = {},
     scenes = ko.observableArray([]),
@@ -57,5 +104,7 @@ $(function () {
 
   my.vm.load();
   ko.applyBindings(my.vm);
+  ko.applyBindings(googleMap);
+
 });
 
