62290090730789e447dfa8247cd56a5fdd79a54e
“Klammertime” <audrey.ck@gmail.com>
2016-03-05 13:30:35 -0800

    chore: README.md includes resources and link to project.

---
+++ README.md
@@ -0,0 +1,43 @@
+##View it live here: 
+[http://klammertime.github.io/map/](http://klammertime.github.io/map/)
+
+
+###Resources
+
+I used the following resources:
+
+* Removing markers
+
+https://developers.google.com/maps/documentation/javascript/examples/marker-remove
+
+* Autocomplete Form
+
+https://blogs.oracle.com/geertjan/entry/html5_datalist_with_knockout_js
+
+* Value vs. TextInput
+
+http://knockoutjs.com/documentation/textinput-binding.html
+
+* This is the best documentation I've ever used. 
+
+[Google Maps JavaScript API](https://developers.google.com/maps/documentation/javascript/)
+
+
+* [Pluralsight - Google Maps API: Get Started](https://app.pluralsight.com/library/courses/google-maps-api-get-started/table-of-contents)
+
+While the general instructions are all relevant, this is from 2014, therefore
+some of the Google Maps API code has been updated. Refer to the docs
+for specific code but you can use this for general tips and advice as I 
+did. He's a good teacher. 
+
+* [Treehouse - Adding a Dynamic Map by Andrew Chalkey](https://teamtreehouse.com/library/build-an-interactive-website/google-maps-integration/adding-a-dynamic-map-2)
+
+This tutorial is also helpful but old.
+Video -  Adding a Dynamic Map:
+This showed me how to make the map responsive and move its center based on the window resize event. When the map is idle, the current center is stored.  
+
+JS Fiddle
+* http://jsfiddle.net/stesta/2T3Db/
+
+
+
--- dist/js/map.js
+++ dist/js/map.js
@@ -1,61 +1,192 @@
-$(function () {
+$(function() {
+    var googleMap,
+        geocoder,
+        map;
 
-  // Location construction
-  var Scene = function() {
-      this.filmLocation = ko.observable();
-      this.filmTitle = ko.observable();
-      this.year = ko.observable();
-      this.director = ko.observable();
-      this.productionCompany = ko.observable();
-      this.writer = ko.observable();
-      // this.latLng = ko.computed(function() {
-      //   return geocode(this.location());
-      // }, this);
-      this.latLng = null;
-      this.description = null;
-      this.wikipedia = null;
-      this.flickr = null;
-      this.nyTimes = null;
-      this.review = null;
-  };
+    // Location construction
+    var Scene = function() {
+        this.filmLocation = ko.observable();
+        this.displayLocation = ko.observableArray();
+        this.filmTitle = ko.observable();
+        this.year = ko.observable();
+        this.director = ko.observable();
+        this.productionCompany = ko.observable();
+        this.writer = ko.observable();
+        this.latLng = null;
+        this.latLng = null;
+        this.description = null;
+        this.wikipedia = null;
+        this.flickr = null;
+        this.nyTimes = null;
+        this.review = null;
+    };
 
-  my.vm = function() {
-    var metadata = {},
-    scenes = ko.observableArray([]),
-    selectedScenes = ko.observableArray([]),
-    favoritedScenes = ko.observableArray([]),
-    allTitles = ko.observableArray([]),
-    selectedFilm = ko.observableArray([]),
+    ko.bindingHandlers.googleMap = {
+        init: function(element, valueAccessor) {
+            var myLatLng = new google.maps.LatLng(37.77493, -122.419416);
+            var mapOptions = {
+                center: myLatLng,
+                zoom: 13,
+                disableDefaultUI: true,
+                zoomControl: true,
+                panControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                rotateControl: true,
+                overviewMapControl: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var initialCenter = mapOptions.center;
+            var initialZoom = mapOptions.zoom;
+            map = new google.maps.Map(element, mapOptions);
 
-    load = function() {
-      $.each(my.filmData.data.Scenes, function(i, p) {
-        my.vm.scenes.push(new Scene()
-                .filmLocation(p.film_location)
-                .filmTitle(p.film_title)
-                .year(p.release_year)
-                .director(p.director)
-                .productionCompany(p.production_company)
-                .writer(p.writer)
-                );
-        my.vm.allTitles.push(p.film_title);
-      });
-    },
-    uniqueTitles = ko.computed(function() {
-      return ko.utils.arrayGetDistinctValues(allTitles().sort());
-    });
+            addGoToInitialExtent(map, initialCenter, initialZoom);
 
-  return {
-    scenes: scenes,
-    selectedScenes: selectedScenes,
-    load: load,
-    uniqueTitles: uniqueTitles,
-    allTitles: allTitles,
-    selectedFilm:selectedFilm
-  };
-} ();
+            google.maps.event.addDomListener(map, 'idle', function() {
+                center = map.getCenter();
+            });
+
+            // when right click, go back to initial center
+            function addGoToInitialExtent(map, initialCenter, initialZoom) {
+                google.maps.event.addListener(map, 'rightclick', function() {
+                    map.setCenter(initialCenter);
+                    map.setZoom(initialZoom);
+                });
+            }
+        },
+        update: function(element, valueAccessor, allBindings) {
+            window.addEventListener('resize', (function() {
+                map.setCenter(center);
+            }), false);
+            google.maps.event.addDomListener(map, 'click', function() {
+                center = map.getCenter();
+            });
+        }
+    };
+
+    my.vm = function() {
+        var metadata = {},
+            scenes = ko.observableArray([]),
+            currentScenes = ko.observableArray([]), //put current selected film locs
+            selectedScenes = ko.observableArray([]),
+            favoritedScenes = ko.observableArray([]),
+            allTitles = ko.observableArray([]),
+            selectedFilm = ko.observableArray([]),
+            singleFilm = ko.observable(),
+            newFilm = ko.observable(true);
+        var markers = [],
+
+            load = function() {
+                $.each(my.filmData.data.Scenes, function(i, p) {
+                    scenes.push(new Scene()
+                        .displayLocation(p.film_location)
+                        .filmLocation(p.film_location + ", San Francisco, CA")
+                        .filmTitle(p.film_title)
+                        .year(p.release_year)
+                        .director(p.director)
+                        .productionCompany(p.production_company)
+                        .writer(p.writer)
+                    );
+                    allTitles.push(p.film_title);
+                });
+            },
+
+            uniqueTitles = ko.computed(function() {
+                return ko.utils.arrayGetDistinctValues(allTitles().sort());
+            }),
+
+            checkReset = function() {
+                if (markers.length > 0) {
+                    for (var j = 0; j < markers.length; j++) {
+                        my.vm.markers[j].setMap(null);
+                    }
+                    my.vm.currentScenes([]);
+                }
+            },
+
+            codeAddress = function() {
+                this.checkReset();
+                var address;
+                var geocoder = new google.maps.Geocoder();
+                var prev_infowindow = false;
+
+                function masterGeocoder(geocodeOptions1) {
+                    geocoder.geocode(geocodeOptions1, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            map.setCenter(results[0].geometry.location);
+
+                            var contentString = '<div id="content"><p>' +
+                                results[0].formatted_address + '</p></div>';
+
+                            var infowindow = new google.maps.InfoWindow({
+                                content: contentString,
+                                maxWidth: 400
+                            });
+
+                            // this adds a marker to the map
+                            var marker = new google.maps.Marker({
+                                map: map,
+                                position: results[0].geometry.location,
+                                title: results[0].formatted_address,
+                                animation: google.maps.Animation.DROP
+                            });
 
 
-  my.vm.load();
-  ko.applyBindings(my.vm);
+                            marker.addListener('click', function() {
+                                if (prev_infowindow) {
+                                    prev_infowindow.close();
+                                }
+
+                                prev_infowindow = infowindow;
+                                infowindow.open(map, marker);
+                            });
+
+                            markers.push(marker);
+
+                        } else {
+                            console.log("Geocode was not successful for the following reason: " + status);
+                        }
+                    });
+                }
+
+                for (var i = 0; i < this.scenes().length; i++) {
+                    if (singleFilm() == my.vm.scenes()[i].filmTitle()) {
+                        address = my.vm.scenes()[i].filmLocation();
+                        currentScenes.push(address);
+                        var geocodeOptions = {
+                            address: address,
+                            componentRestrictions: {
+                                country: 'US'
+                            }
+                        };
+                        masterGeocoder(geocodeOptions);
+                    }
+                }
+            };
+
+        return {
+            scenes: scenes,
+            selectedScenes: selectedScenes,
+            load: load,
+            uniqueTitles: uniqueTitles,
+            allTitles: allTitles,
+            selectedFilm: selectedFilm,
+            singleFilm: singleFilm,
+            newFilm: newFilm,
+            codeAddress: codeAddress,
+            currentScenes: currentScenes,
+            markers: markers,
+            checkReset: checkReset
+        };
+    }();
+
+    my.vm.load();
+
+    // my.vm.singleFilm.subscribe(function() {
+    //   my.vm.codeAddress();
+    // }, my.vm);
+
+
+    ko.applyBindings(my.vm);
+
 });
-
