6feb5c9015e7639f6a7ca05a2164a821f90bb8d6
“Klammertime” <audrey.ck@gmail.com>
2016-03-21 11:59:45 -0700

    refactor: change checkReset so when you click the same movie in succession nothing happens.
    Before, the markers were clearing again but less addresses were added if you chose the same film twice.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 $(function() {
     'use strict';
-    var geocoder;
-    var map;
-    var infowindow;
-    var prev_infowindow = false;
+    var prev_infowindow = false,
+        geocoder,
+        map,
+        infowindow;
 
     function googleSuccess() {
-        var center;
-        var myLatLng = new google.maps.LatLng(37.77493, -122.419416);
+        var center,
+            myLatLng = new google.maps.LatLng(37.77493, -122.419416);
         var mapOptions = {
             center: myLatLng,
             zoom: 12,
@@ -33,8 +33,8 @@ $(function() {
             scrollwheel: false, // prevents mousing down from triggering zoom
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var initialCenter = mapOptions.center;
-        var initialZoom = mapOptions.zoom;
+        var initialCenter = mapOptions.center,
+            initialZoom = mapOptions.zoom;
         map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
         addGoToInitialExtent(map, initialCenter, initialZoom);
@@ -63,7 +63,7 @@ $(function() {
 
     // Location construction
     var SceneFilmModel = Base.extend({
-        constructor: function(director, studio, fullAddress, place, streetAddress, year, filmTitle, writer, favorite, actor1, actor2, actor3, funFact) {
+        constructor: function(director, studio, fullAddress, place, streetAddress, year, filmTitle, writer, actor1, actor2, actor3, funFact) {
             this.director = ko.observable(director);
             this.studio = ko.observable(studio);
             this.fullAddress = ko.observable(fullAddress);
@@ -71,77 +71,68 @@ $(function() {
             this.streetAddress = ko.observable(streetAddress);
             this.year = ko.observable(year);
             this.filmTitle = ko.observable(filmTitle);
-            this.writer = ko.observable(writer);
-            this.favorite = ko.observable(false);
             this.actor1 = ko.observable(actor1);
             this.actor2 = ko.observable(actor2);
             this.actor3 = ko.observable(actor3);
+            this.writer = ko.observable(writer);
             this.funFact = ko.observable(funFact);
         }
     });
 
     function titleCheck(theData, resultsTitleProp, resultsDateProp, desiredTitle, desiredYear) {
-        var resultsLength = theData.results.length;
-
         function justYear(longDate) {
-            var myRegexp = /[^-]*/;
-            var match = myRegexp.exec(longDate);
-            return match;
+            var match = /[^-]*/.exec(longDate);
+            console.log("match", match);
+            return match[0];
         }
 
-        for (var i = 0; i < resultsLength; i++) {
-            var resultsTitle = theData.results[i][resultsTitleProp];
-            var resultsYear = justYear(theData.results[i][resultsDateProp]);
-            resultsYear = resultsYear[0];
-
-            if ((resultsTitle == desiredTitle) && (resultsYear == desiredYear)) {
+        for (var i = 0, r = theData.results.length; i < r; i++) {
+            if (((theData.results[i][resultsTitleProp]) == desiredTitle) && (justYear(theData.results[i][resultsDateProp]) == desiredYear)) {
                 return i;
             }
         }
     }
 
     my.vm = function() {
-        var scenes = ko.observableArray([]);
-        var currentScenes = ko.observableArray([]); //put current selected film locs
-        var favoritedScenes = ko.observableArray([]);
-        var allTitles = ko.observableArray([]);
-        var requestedFilm = ko.observable();
-        var markers = ko.observableArray([]);
-        var overview = ko.observable();
-        var tagline = ko.observable();
-        var trailerURL = ko.observable();
-        var currentTitle = ko.observable();
-        var currentYear = ko.observable();
-        var currentDirector = ko.observable();
-        var currentWriter = ko.observable();
-        var currentActor1 = ko.observable();
-        var currentActor2 = ko.observable();
-        var currentActor3 = ko.observable();
-        var currentStudio = ko.observable();
-        var posterSRC = ko.observable();
-        var nytInfo = ko.observableArray([]);
-        var movieDBInfo = ko.observableArray([]);
-        var moviedb = ko.observable();
-
-        var loadSceneFM = function() {
+        var scenes = ko.observableArray([]),
+            currentScenes = ko.observableArray([]), //put current selected film locs
+            allTitles = ko.observableArray([]),
+            requestedFilm = ko.observable(),
+            pastFilm = ko.observable(),
+            markers = ko.observableArray([]),
+            overview = ko.observable(),
+            tagline = ko.observable(),
+            trailerURL = ko.observable(),
+            currentTitle = ko.observable(),
+            currentYear = ko.observable(),
+            currentDirector = ko.observable(),
+            currentWriter = ko.observable(),
+            currentActor1 = ko.observable(),
+            currentActor2 = ko.observable(),
+            currentActor3 = ko.observable(),
+            currentActors = ko.observableArray([]),
+            currentStudio = ko.observable(),
+            posterSRC = ko.observable(),
+            nytInfo = ko.observableArray([]),
+            movieDBInfo = ko.observableArray([]),
+            moviedb = ko.observable(),
+            loadSceneFM = function() {
                 // Returns everything before first parentheses, which is the place
                 function escapeRegExp(string) {
-                    var regExp = /^.*?(?=\s\()/;
-                    var matches = regExp.exec(string);
+                    var matches = /^.*?(?=\s\()/.exec(string);
                     return matches ? matches[0] : undefined;
                 }
 
                 // Returns everything between the parentheses, which is the street address
                 // and is the format Google Maps JS API for Geolocation prefers
                 function escapeRegExp2(string) {
-                    var regExp2 = /\(([^)]+)\)/;
-                    var matches = regExp2.exec(string);
+                    var matches = /\(([^)]+)\)/.exec(string);
                     return matches ? matches[1] : undefined;
                 }
 
                 $.each(my.filmData.data.Scenes, function(i, s) { //s stands for 'scene'
                     if (s.film_location !== undefined) { // create more false conditions
-                        scenes.push(new SceneFilmModel(s.director, s.production_company, s.film_location, escapeRegExp(s.film_location), escapeRegExp2(s.film_location), s.release_year, s.film_title, s.writer, false, s.actor_1, s.actor_2, s.actor_3, s.fun_facts));
+                        scenes.push(new SceneFilmModel(s.director, s.production_company, s.film_location, escapeRegExp(s.film_location), escapeRegExp2(s.film_location), s.release_year, s.film_title, s.writer, s.actor_1, s.actor_2, s.actor_3, s.fun_facts));
                         allTitles.push(s.film_title);
                     }
                 });
@@ -155,14 +146,14 @@ $(function() {
                 googleSuccess();
             },
 
-            checkReset = function() {
-                if (this.markers().length > 0) {
-                    $.each(this.markers(), function(i, marker) {
-                        marker.marker.setMap(null);
-                    });
-                    this.markers([]);
-                }
-            },
+
+
+            // roadFilter = ko.computed(function() {
+            //     var matchedLocs = ko.utils.arrayFilter(this.markers(), function(marker) {
+            //         return marker.title == this.filterWord();
+            //     });
+            //     //? return matchedLocs
+            // }),
 
             // The current item will be passed as the first parameter
             panToMarker = function(clickedLocation) {
@@ -179,16 +170,14 @@ $(function() {
             },
 
             loadNYTData = function(encodedFilm, nonEncodedFilm, releaseYear) {
-                var index1;
-                var nytKey = '70f203863d9c8555f9b345f32ec442e8:10:59953537';
-                var nytAPI = "http://api.nytimes.com/svc/movies/v2/reviews/search.json?query='" +
-                    encodedFilm + "'&api-key=" + nytKey;
+                var index1,
+                    nytAPI = "http://api.nytimes.com/svc/movies/v2/reviews/search.json?query='" +
+                    encodedFilm + "'&api-key=70f203863d9c8555f9b345f32ec442e8:10:59953537";
                 nytInfo(undefined);
 
 
                 function domain(fullUrl) {
-                    var myRegexp = /\.(.*)$/; // get everything after first period
-                    var match = myRegexp.exec(fullUrl);
+                    var match = /\.(.*)$/.exec(fullUrl);
                     return 'http://www.' + match[1]; //it works just not while on local server
                 }
 
@@ -231,9 +220,9 @@ $(function() {
 
                 theMovieDb.search.getMovie({ "query": encodedFilm },
                     (function(data) {
-                        var filmID;
-                        var index;
-                        var dbStore = JSON.parse(data);
+                        var filmID,
+                            index,
+                            dbStore = JSON.parse(data);
                         moviedb(dbStore);
                         console.log("moviedb()", moviedb());
                         if ((moviedb().results[0].original_title.toLowerCase() == nonEncodedFilm.toLowerCase()) || (moviedb().results[0].original_title == ('The ' + nonEncodedFilm))) {
@@ -241,7 +230,7 @@ $(function() {
                         } else {
                             index = titleCheck(dbStore, 'original_title', 'release_date', nonEncodedFilm, releaseYear);
                             console.log("index in moviedb", index);
-                            if(index === undefined) {
+                            if (index === undefined) {
 
                             }
                         }
@@ -271,25 +260,26 @@ $(function() {
                     (function() {
                         console.log("you fail!"); //TODO: find the proper error
                     }));
-            testDB();
+                testDB();
 
             },
 
-           testDB = function() {
-                function successCB(data){
-                    console.log("test data", data);
+            testDB = function() {
+                function successCB(data) {
+                    // console.log("test data", data);
                 }
-                function errorCB(data){
+
+                function errorCB(data) {
                     console.log("test errorCB");
                 }
 
-                theMovieDb.collections.getCollection({"id": 10, "append_to_response": "trailers"}, successCB, errorCB);
+                theMovieDb.collections.getCollection({ "id": 10, "append_to_response": "trailers" }, successCB, errorCB);
 
-           },
+            },
 
             masterGeocoder = function(myGeocodeOptions, place, geocoder) {
-                var contentString;
-                var marker;
+                var contentString,
+                    marker;
                 geocoder.geocode(myGeocodeOptions, function(results, status) {
                     if (status == google.maps.GeocoderStatus.OK) {
                         map.setCenter(results[0].geometry.location);
@@ -345,32 +335,54 @@ $(function() {
                     }
                 });
             },
+            checkReset = function() {
+                console.log("checkReset fired");
+                console.log("pastFilm() in checkReset", pastFilm());
+                console.log("requestedFilm() in checkReset", requestedFilm());
+
+                if ((this.markers().length > 0) && (pastFilm() !== requestedFilm())) {
+                    console.log("(pastFilm() !== requestedFilm())", pastFilm() !== requestedFilm());
+                    $.each(this.markers(), function(i, marker) {
+                        marker.marker.setMap(null);
+                    });
+                    this.markers([]);
+                    return true;
+                } else if (!pastFilm()) {
+                    console.log("!pastFilm()", !pastFilm());
+                    return true;
+                } else {
+                    console.log("its returning false");
+                    return false;
+                }
+            },
 
             codeAddress = function() {
-                var address;
-                var place;
-                var matchedScene;
-                var matchedTitle;
-                var matchedYear;
-                var scenesLength = this.scenes().length;
-                this.checkReset();
-                    //TODO change to foreach
-                    for (var i = 0; i < scenesLength; i++) {
-                        if (requestedFilm().toLowerCase().trim() == this.scenes()[i].filmTitle().toLowerCase().trim()) {
-                            matchedScene = this.scenes()[i];
-                            matchedTitle = this.scenes()[i].filmTitle();
-                            matchedYear = this.scenes()[i].year();
+                var address,
+                    place,
+                    matchedScene,
+                    matchedTitle,
+                    matchedYear;
+                console.log("codeAddress fired");
+
+                if (this.checkReset()) {
+                    console.log("scene area firing");
+                    for (var j = 0, s = this.scenes().length; j < s; j++) {
+                        if (requestedFilm().toLowerCase().trim() == this.scenes()[j].filmTitle().toLowerCase().trim()) {
+                            matchedScene = this.scenes()[j];
+                            matchedTitle = this.scenes()[j].filmTitle();
+                            matchedYear = this.scenes()[j].year();
                             var geocoder = new google.maps.Geocoder();
 
-                            if (this.scenes()[i].place()) {
-                                address = this.scenes()[i].streetAddress();
-                                place = this.scenes()[i].place();
+                            if (this.scenes()[j].place()) {
+                                address = this.scenes()[j].streetAddress();
+                                place = this.scenes()[j].place();
                             } else {
-                                address = this.scenes()[i].fullAddress();
+                                address = this.scenes()[j].fullAddress();
                                 place = undefined;
                             }
 
-                            currentScenes.push(this.scenes()[i]);
+                            //TODO: you don't do anything with this!
+                            currentScenes.push(this.scenes()[j]);
 
                             var geocodeOptions = {
                                 address: address + ', San Francisco, CA',
@@ -381,17 +393,20 @@ $(function() {
                             masterGeocoder(geocodeOptions, place, geocoder);
                         } // end of master if statement
                     }
+                    console.log("reaching bottom of codeAddress");
+                    this.currentTitle(matchedTitle);
+                    this.currentYear(matchedYear);
+                    this.currentDirector(matchedScene.director());
+                    this.currentWriter(matchedScene.writer());
+                    this.currentActor1(matchedScene.actor1());
+                    this.currentActor2(matchedScene.actor2());
+                    this.currentActor3(matchedScene.actor3());
+                    this.currentStudio(matchedScene.studio());
+                    loadNYTData(encodeURIComponent(matchedTitle), matchedTitle, matchedYear);
+                    loadMovieDbData(encodeURIComponent(matchedTitle), matchedTitle, matchedYear);
+                    pastFilm(requestedFilm());
+                }
 
-                this.currentTitle(matchedTitle);
-                this.currentYear(matchedYear);
-                this.currentDirector(matchedScene.director());
-                this.currentWriter(matchedScene.writer());
-                this.currentActor1(matchedScene.actor1());
-                this.currentActor2(matchedScene.actor2());
-                this.currentActor3(matchedScene.actor3());
-                this.currentStudio(matchedScene.studio());
-                loadNYTData(encodeURIComponent(matchedTitle), matchedTitle, matchedYear);
-                loadMovieDbData(encodeURIComponent(matchedTitle), matchedTitle, matchedYear);
                 //TODO: do an alert for the wrong film, html binding? <div class="alert alert-danger" role="alert">...</div>
                 // This page might help: https://www.safaribooksonline.com/library/view/knockoutjs-by-example/9781785288548/ch02s04.html
             };
@@ -432,4 +447,4 @@ $(function() {
     ko.observable.fn.equalityComparer = function(a, b) {
         return a === b;
     };
-});
+})
